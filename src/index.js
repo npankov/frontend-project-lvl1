17d95c7905b2b35e87983a8273380a488c2aee8f
n.pankov <pankoff2000@gmail.com>
2019-10-12 12:36:09 +0200

    4eme pas

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+console.log(`Hi ${userName}!`);
