ddead5b92d2cdaa04b10e1a76c15bbe28686f23d
panek <you@example.com>
2019-12-02 15:37:28 +0100

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 // min and max included
-const randomNum = (max, min) => Math.floor(Math.random() * (max - min) + min);
+const randomNum = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
 export default randomNum;
